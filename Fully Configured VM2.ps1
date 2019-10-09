7e71044cc7202db223037da3ac72772007314252
sahiray <43626422+sahiray@users.noreply.github.com>
2019-10-08 20:17:41 -0400

    Update Fully Configured VM2.ps1

--- Fully Configured VM2.ps1
+++ Fully Configured VM2.ps1
@@ -1,13 +1,13 @@
 # Variables for common values
-$DC2resourceGroup = "DC-3"
+$DC2resourceGroup = "DC-4"
 $locationeastus2 = "eastus"
 
 # Create a resource group
 New-AzResourceGroup -Name $DC2resourceGroup -Location $locationeastus2
 
 # Create a subnet configuration
-$subnetConfigdc3 = New-AzVirtualNetworkSubnetConfig -Name Subnet1 -AddressPrefix 10.10.1.0/24
+$subnetConfigdc4 = New-AzVirtualNetworkSubnetConfig -Name Subnet1 -AddressPrefix 49.10.10.0/24
 
 # Create a virtual network
-$vnetdc2 = New-AzVirtualNetwork -ResourceGroupName $DC2resourceGroup -Location $locationeastus2 `
-  -Name VNET-DC3 -AddressPrefix 10.10.0.0/16 -Subnet $subnetConfigdc3
+$vnetdc4 = New-AzVirtualNetwork -ResourceGroupName $DC2resourceGroup -Location $locationeastus2 `
+  -Name VNET-DC4 -AddressPrefix 49.10.0.0/16 -Subnet $subnetConfigdc3
